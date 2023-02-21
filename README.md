b22581a2b77314f51acf0a6adba6888444defdce
Nate Wichman <nwichman@ostusa.com>
2023-02-21 11:03:19 -0500

    basic

---
+++ README.md
@@ -0,0 +1,9 @@
+## Remove All Local Git Branches
+
+# Usage
+
+`npm i vanbonn-clean-git -g`
+
+`vanbonn-clean-git` - removes all branches except 'develop'
+
+`vanbonn-clean-git main` - removes all branches except 'main'
---
+++ bin/index.js
@@ -0,0 +1,23 @@
+// git branch | grep -v \"develop\" | xargs git branch -D
+
+
+const { exec } = require("child_process")
+
+const args = process.argv.slice(2)
+
+
+const branch = args[0] || 'develop'
+
+console.log('\x1b[35m', 'removing all local git branches except ' + branch + ' - 🦐')
+
+exec(`git branch | grep -v \"${branch}\" | xargs git branch -D`, (error, stdout, stderr) => {
+    if (error) {
+        console.log('\033[31m', `error: ${error.message}`)
+        return
+    }
+    if (stderr) {
+        console.log('\033[31m', `stderr: ${stderr}`)
+        return
+    }
+    console.log(`stdout: ${stdout}`)
+})
